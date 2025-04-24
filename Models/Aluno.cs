0c07da8fdd39d7e1bbc963781959634decceb806
Miguel de M Vicari <miguelmvjiujitsu@gmail.com>
2025-04-24 08:50:19 -0300

    Aviso

--- Models/Aluno.cs
+++ Models/Aluno.cs
@@ -5,7 +5,6 @@ namespace ProjetoCorina2.Models
     public class Aluno
     {
         public Guid AlunoId { get; set; }
-
         public string Nome { get; set; }
 
         public string Email { get; set; }
---
+++ Models/Aviso.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjetoCorina2.Models
+{
+    public class Aviso
+    {
+        public Guid AvisoId { get; set; }
+        public string Descricao { get; set; }
+        public Guid AlunoId { get; set; }
+    }
+}
